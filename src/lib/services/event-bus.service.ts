2673a5db0bae79f9fcdfcc0276f43538b15684f1
Michael Souto <michael.souto@detrasoft.com>
2025-04-14 10:01:04 -0300

    Implementação de eventbus

--- src/lib/services/event-bus.service.ts
+++ src/lib/services/event-bus.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { filter, map, tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class EventBusService {
-  private events$ = new Subject<{ type: string; payload?: any }>();
+  private events$ = new Subject<Event>();
 
-  emit(event: { type: string; payload?: any }) {
+  emit(event: Event) {
     this.events$.next(event);
   }
 
   on(type: string) {
     return this.events$.asObservable().pipe(
       filter((event) => event.type === type),
-      map((event) => event.payload)
+      map((event) => event),
     );
   }
+}
+
+export interface Event {
+  type: string;
+  payload?: any;
+  callback?: (payload: any) => void;
+  valid?: (payload: any) => void;
 }
--- src/lib/services/framework.service.ts
+++ src/lib/services/framework.service.ts
@@ -2,6 +2,7 @@ import { Location } from "@angular/common";
 import { Injectable } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { UtilsService } from "./utils/utils.service";
+import { EventBusService } from "./event-bus.service";
 
 @Injectable({
   providedIn: "root"
@@ -10,7 +11,8 @@ export class FrameworkService {
   constructor(
     public location: Location,
     public router: Router,
-    public utils: UtilsService
+    public utils: UtilsService,
+    public eventBusService: EventBusService
   ) {}
 
   getLanguageMap() {
